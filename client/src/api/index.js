1eecfc3ba7e25687061c5e6e6d86056c8b2baf38
Medha Shree <152624687+Medha170@users.noreply.github.com>
2025-06-25 00:57:45 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://network-relaibilty-visualizer.onrender.com',
+  baseURL: 'https://network-relaibilty-visualizer.onrender.com/api',
 });
 
 export default api;
